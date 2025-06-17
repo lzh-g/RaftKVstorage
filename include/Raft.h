587f43c5f60fe2fd746f90c763e3a31ea92731f9
lzh-g <2232284193@qq.com>
2025-06-17 23:40:57 +0800

    Init project

---
+++ include/Raft.h
@@ -0,0 +1,16 @@
+#ifndef RAFT_H_
+#define RAFT_H_
+
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/vector.hpp>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+
+#endif // !RAFT_H_
